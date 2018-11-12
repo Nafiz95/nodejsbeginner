5c924bd08acc69a52fc2a681ce0e38e7d54a048c
Nafiz Sadman <nafizsadman420@gmail.com>
2018-11-12 03:04:55 +0000

    class work 8

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 var http = require('http');
-var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+var express= require('express');
+var app= express();
+var server=http.Server(app);
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+server.listen(process.env.PORT || 3000, process.env.IP|| 'localhost', function(){
+     console.log('Server running');
+ });
+
+//   var server = http.createServer(function(req, res){
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
     
-    fs.readFile('index.html',function(err,data)
-    {
-      if(err){
-          return console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOB FILE READ ERROR");
-      }  
-      res.end(data);
-    });
+//     fs.readFile('index.html',function(err,data)
+//     {
+//       if(err){
+//           return console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOB FILE READ ERROR");
+//       }  
+//       res.end(data);
+//     });
     
-    //res.end("Hello World\n");
-  });
-  server.listen(process.env.PORT || 3000, process.env.IP|| 'localhost', function(){
-    console.log('Server running');
-  });
+//     //res.end("Hello World\n");
+//   });
+//   server.listen(process.env.PORT || 3000, process.env.IP|| 'localhost', function(){
+//     console.log('Server running');
+
+
+
+
