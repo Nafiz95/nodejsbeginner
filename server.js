cc5a90d34edfacf32c6c43efd23f99b0edc23c42
Nafiz Sadman <nafizsadman420@gmail.com>
2018-11-19 15:34:50 +0000

    CW9

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var http = require('http');
-var express= require('express');
-var app= express();
-var server=http.Server(app);
-var bodyParser=require('body-parser');
-
+  var http = require('http');
+  var express = require('express');
+  var app = express();
+  var server = http.Server(app);
+  var bodyParser = require('body-parser');
+  
+  var mongo = require('mongodb');
+  
+  //For C9
+  var db;
+  var db_url = "mongodb://"+process.env.IP+":27017"
+  mongo.MongoClient.connect(db_url, {useNewUrlParser: true}, function(err, client){
+  if(err){
+    console.log('Could not connect to MongoDB');
+  }else{
+    db= client.db('node-cw9');
+  }  
+  })
+  
+  var save = function(form_data){
+    db.createCollection('articles', function(err, collection){
+      
+    })
+    var collection = db.collection('articles');
+    collection.save(form_data);
+  };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -32,7 +52,8 @@ app.post('/article/create',function(req,res)
     {
         return res.status(400).json({error:"ADD CONTENT NOOB"});
     }
-    article.push(req.body);
+    // article.push(req.body);
+    save(request.body);
     return res.status('200').json({message:"TA_DA"});
 });
 
