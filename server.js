955217d03ea578e977fa89fc4b1d7f019f81f397
Nafiz Sadman <nafizsadman420@gmail.com>
2018-11-22 15:47:34 +0000

    cw9c

--- server.js
+++ server.js
@@ -1,95 +1,33 @@
-  var http = require('http');
-  var express = require('express');
-  var app = express();
-  var server = http.Server(app);
-  var bodyParser = require('body-parser');
-  
-  var mongo = require('mongodb');
-  
-  //For C9
-  var db;
-  var db_url = "mongodb://"+process.env.IP+":27017"
-  mongo.MongoClient.connect(db_url, {useNewUrlParser: true}, function(err, client){
-  if(err){
-    console.log('Could not connect to MongoDB');
-  }else{
-    db= client.db('node-cw9');
-  }  
-  })
-  
-  var save = function(form_data){
-    db.createCollection('articles', function(err, collection){
-      
-    })
-    var collection = db.collection('articles');
-    collection.save(form_data);
-  };
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+
+// var Article = require('./models/article.js');
+
+var db;
+var db_url = "mongodb://"+process.env.IP+":27017";
+
+//CW9b
+mongoose.connect(db_url+"/node-cw9");
+mongoose.connection.on('error', function(){
+  console.log('Could not connect to MongoDB');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+app.get('/', function(request,response){
+  response.render('index.ejs');
 });
 
-app.get('/About',function(req,res){
-  res.sendFile(__dirname+'/About.html');
+app.get('/about', function(request,response){
+  response.sendFile('about.ejs');
 });
 
-app.get('/form',function(req,res){
-  res.sendFile(__dirname+'/form.html');
+require('./routes/article-routes.js')(app);
+server.listen(process.env.PORT, process.env.IP, function(){
+  console.log('Server running');
 });
-
-var article= [];
-app.post('/article/create',function(req,res)
-{
-    console.log(req.body);
-    if(!req.body.title)
-    {
-        return res.status(400).json({error:"ADD A TITLE NOOB"});
-    }
-    
-    else if(!req.body.content)
-    {
-        return res.status(400).json({error:"ADD CONTENT NOOB"});
-    }
-    // article.push(req.body);
-    save(request.body);
-    return res.status('200').json({message:"TA_DA"});
-});
-
-app.get('/article/list',function(req, res) {
-    return res.status(200).json({articles: article});
-})
-
-  article.push({title: "Test Article 1", content:"Content 1"});
-  article.push({title: "Test Article 2", content:"Content 2"});
-  app.get('/article/:articleID', function(request, response) {
-      response.render('../article.ejs',{
-        article: article[request.params.articleID]
-      })
-  });
-
-server.listen(process.env.PORT || 3000, process.env.IP|| 'localhost', function(){
-     console.log('Server running');
- });
-
-//   var server = http.createServer(function(req, res){
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-    
-//     fs.readFile('index.html',function(err,data)
-//     {
-//       if(err){
-//           return console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOB FILE READ ERROR");
-//       }  
-//       res.end(data);
-//     });
-    
-//     //res.end("Hello World\n");
-//   });
-//   server.listen(process.env.PORT || 3000, process.env.IP|| 'localhost', function(){
-//     console.log('Server running');
-
-
-
-
